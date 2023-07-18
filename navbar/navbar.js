3818858a5c7481dce6d845bc6b2b18d35ab92d2c
Dominic H <75951207+DQMME@users.noreply.github.com>
2023-07-18 08:37:15 +0200

    Create navbar.js

---
+++ navbar/navbar.js
@@ -0,0 +1,8 @@
+function openNavbar() {
+  const navbar = document.getElementById("navbar");
+  if (navbar.className === "navbar") {
+    navbar.className += " responsive";
+  } else {
+    navbar.className = "navbar";
+  }
+}
